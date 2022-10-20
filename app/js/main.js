738e6ca8b8a282f8c3891942ba0dbe371a3c99f0
Egor <beltyugov94@mail.com>
2022-10-20 18:37:02 +0300

    пушкоммит

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
 
 $(function () {
-    $('.header__menu').click(function () {
-      $('.menu,.header__menu').toggleClass('show-menu')
-    })
-  });
+  $('.header__menu').click(function () {
+    $('.menu,.header__menu').toggleClass('show-menu')
+  })
+});
+
+
+// $(function () {
+//   $('.menu__list').click(function () {
+//     $('.menu__sub-list').toggleClass('sub-menu')
+//   })
+// });
