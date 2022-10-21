64b8cba0d3ea164d0d82ba5e8f973312eb0a88d2
Egor <beltyugov94@mail.com>
2022-10-21 21:16:58 +0300

    new

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,13 @@
 
 $(function () {
   $('.header__menu').click(function () {
-    $('.menu,.header__menu').toggleClass('show-menu')
+    $('.menu,.header__menu, .hero__block').toggleClass('show-menu')
   })
 });
 
 
-// $(function () {
-//   $('.menu__list').click(function () {
-//     $('.menu__sub-list').toggleClass('sub-menu')
-//   })
-// });
+$(function () {
+  $('.menu__list').click(function () {
+    $('.menu__sub-list').toggleClass('sub-menu')
+  })
+});
